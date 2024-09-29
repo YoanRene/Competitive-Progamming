2732a7c78c9a3af57b2834cee3a001b1c0a4a72a
Yoan René Ramos Corrales <106040694+YoanRene@users.noreply.github.com>
2024-09-29 02:33:47 -0400

    Update DSA.cpp

--- DSA.cpp
+++ DSA.cpp
@@ -214,140 +214,6 @@ struct ST
     }
 };
 
-
-//Segment Tree Lazy
-int tree[4*100001] = {0};  
-int lazy[4*100001] = {0};  
- 
-void updateRangeUtil(int si, int ss, int se, int us, int ue, int diff)
-{
-    if (lazy[si] != 0)
-    {
-        tree[si] += (se-ss+1)*lazy[si];
- 
-        if (ss != se)
-        {
-            lazy[si*2 + 1]   += lazy[si];
-            lazy[si*2 + 2]   += lazy[si];
-        }
- 
-        lazy[si] = 0;
-    }
- 
-    if (ss>se || ss>ue || se<us)
-        return ;
- 
-    if (ss>=us && se<=ue)
-    {
-        tree[si] += (se-ss+1)*diff;
- 
-        if (ss != se)
-        {
-            lazy[si*2 + 1]   += diff;
-            lazy[si*2 + 2]   += diff;
-        }
-        return;
-    }
- 
-    int mid = (ss+se)/2;
-    updateRangeUtil(si*2+1, ss, mid, us, ue, diff);
-    updateRangeUtil(si*2+2, mid+1, se, us, ue, diff);
- 
-    tree[si] = tree[si*2+1] + tree[si*2+2];
-}
- 
-void updateRange(int n, int us, int ue, int diff)
-{
-   updateRangeUtil(0, 0, n-1, us, ue, diff);
-}
- 
-int getSumUtil(int ss, int se, int qs, int qe, int si)
-{
-    if (lazy[si] != 0)
-    {
-        tree[si] += (se-ss+1)*lazy[si];
- 
-        if (ss != se)
-        {
-            lazy[si*2+1] += lazy[si];
-            lazy[si*2+2] += lazy[si];
-        }
- 
-        lazy[si] = 0;
-    }
- 
-    if (ss>se || ss>qe || se<qs)
-        return 0;
- 
-    if (ss>=qs && se<=qe)
-        return tree[si];
- 
-    int mid = (ss + se)/2;
-    return getSumUtil(ss, mid, qs, qe, 2*si+1) +
-           getSumUtil(mid+1, se, qs, qe, 2*si+2);
-}
- 
-int getSum(int n, int qs, int qe)
-{
-    if (qs < 0 || qe > n-1 || qs > qe)
-    {
-        cout <<"Invalid Input";
-        return -1;
-    }
- 
-    return getSumUtil(0, n-1, qs, qe, 0);
-}
- 
-void constructSTUtil(int arr[], int ss, int se, int si)
-{
-    if (ss > se)
-        return ;
- 
-    if (ss == se)
-    {
-        tree[si] = arr[ss];
-        return;
-    }
- 
-    int mid = (ss + se)/2;
-    constructSTUtil(arr, ss, mid, si*2+1);
-    constructSTUtil(arr, mid+1, se, si*2+2);
- 
-    tree[si] = tree[si*2 + 1] + tree[si*2 + 2];
-}
- 
-void constructST(int arr[], int n)
-{
-    constructSTUtil(arr, 0, n-1, 0);
-}
- 
-//Sparse Table Esta Mal ,Tengo que revisarla bien
-struct SparseTable
-{
-    vector<vll> lookup;
-
-    SparseTable(vll &arr, int n)
-    {
-        lookup.assign(n,vll(ll(log2(n))+1));
-        rep(0,n){
-            lookup[i][0]=arr[i];
-        }
-
-        for (int j = 1; (1 << j) <= n; j++) {
-            for (int i = 0; (i + (1 << j) - 1) < n; i++) {
-                lookup[i][j]=lookup[i][j - 1]&lookup[i + (1 << (j - 1))][j - 1];
-            }
-        }
-    }
- 
-    // Returns minimum of arr[L..R] 0<=L,R<n
-    int query(int L, int R)
-    {
-        int j = (int)log2(R - L + 1);
-        return lookup[L][j]&lookup[R - (1 << j) + 1][j];
-    }
-};
-
 //String Hashed
 long long compute_hash(string const& s) {
     const int p = 31;
@@ -677,7 +543,34 @@ void Prim_MST(ll x,ll n,vb &seen, vector<vp> &adj,vll &edges){//egdes(n)
     }
 }
 
+//Prim O(|V|^2)
+struct Edge {
+    int w = 4001, to = -1;
+};
+void prim(vector<vector<int>> &adj,ll &n,vb &seen,ll &sum) {
+    int total_weight = 0;
+    vector<Edge> min_e(n);
+    min_e[0].w = 0;
+
+    for (int i=0; i<n; ++i) {
+        int v = -1;
+        for (int j = 0; j < n; ++j) {
+            if (!seen[j] && (v == -1 || min_e[j].w < min_e[v].w))
+                v = j;
+        }
+        seen[v] = true;
+        total_weight += min_e[v].w;
+
+        for (int to = 0; to < n; ++to) {
+            if (adj[v][to] < min_e[to].w)
+                min_e[to] = {adj[v][to], v};
+        }
+    }
+    sum=total_weight;
+}
+
 //MO algotithm (SQRT Descomposition)
+
 struct Query
 {
     ll L, R, i;
